e1e9b49e2b9a851c1bf82a2ecffd60019c92120b
jthielen <github@thielen.science>
2025-04-11 14:37:36 -0600

    Add dewpoint demo py file

---
+++ dewpoint.py
@@ -0,0 +1,10 @@
+import metpy.calc as mpcalc
+from metpy.units import units
+
+mixing = 10 * units('g/kg')
+p0 = 1000 * units.hPa
+
+e = mpcalc.vapor_pressure(p0, mixing)
+td = mpcalc.dewpoint(e)
+
+print(e)
