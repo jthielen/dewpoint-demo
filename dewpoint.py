6d521aa766a371f4fbf03801a217f77ca199bf6f
Bali Summers <144489305+baliksummers@users.noreply.github.com>
2025-04-11 14:48:30 -0600

    Changed values

--- dewpoint.py
+++ dewpoint.py
@@ -1,8 +1,8 @@
 import metpy.calc as mpcalc
 from metpy.units import units
 
-mixing = 10 * units('g/kg')
-p0 = 1000 * units.hPa
+mixing = 23 * units('g/kg')
+p0 = 500 * units.hPa
 
 e = mpcalc.vapor_pressure(p0, mixing)
 td = mpcalc.dewpoint(e)
